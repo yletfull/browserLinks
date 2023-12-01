04291f986a41ec43d2f9b8f3d0e02511b93cefeb
Andrey Korlyakov <akorlyakov@phoenixit.ru>
2023-12-01 22:43:54 +0300

    refac

--- background.js
+++ background.js
@@ -3,9 +3,3 @@ chrome.runtime.onInstalled.addListener(function() {
   console.log('Extension installed');
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'executeCode') {
-    // Добавьте свой код, который нужно выполнить при нажатии кнопки
-    console.log('Button clicked');
-  }
-});
